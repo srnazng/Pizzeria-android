b07f9cb4e84afbcb22399f0c461be69d81daf1e4
srnazng <srnazng@gmail.com>
2022-12-03 18:26:51 -0500

    chicago activity

--- app/src/main/java/com/example/pizzeria/MainActivity.java
+++ app/src/main/java/com/example/pizzeria/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.pizzeria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvChicagoPizza, tvNYPizza;
+    private Button btnStoreOrders, btnCart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvChicagoPizza = findViewById(R.id.tvChicagoPizza);
+        tvChicagoPizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ChicagoActivity.class);
+                startActivity(i);
+            }
+        });
+
+        tvNYPizza = findViewById(R.id.tvNYPizza);
+        tvNYPizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnStoreOrders = findViewById(R.id.btnStoreOrders);
+        btnStoreOrders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnCart = findViewById(R.id.btnCart);
+        btnCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
