c38f6419bf16a50f9d1cbefdf4aec350f8c71a57
srnazng <srnazng@gmail.com>
2022-12-03 23:39:14 -0500

    recycler view

--- app/src/main/java/com/example/pizzeria/ChicagoActivity.java
+++ app/src/main/java/com/example/pizzeria/ChicagoActivity.java
@@ -1,14 +1,33 @@
 package com.example.pizzeria;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class ChicagoActivity extends AppCompatActivity {
+    RecyclerView rvChicagoToppings;
+    ArrayList<Topping> toppings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chicago);
+        rvChicagoToppings = findViewById(R.id.rvChicagoToppings);
+        setupMenuItems(); //add the list of items to the ArrayList
+        ToppingsAdapter adapter = new ToppingsAdapter(this, toppings); //create the adapter
+        rvChicagoToppings.setAdapter(adapter); //bind the list of items to the RecyclerView
+        //use the LinearLayout for the RecyclerView
+        rvChicagoToppings.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    /**
+     * Helper method to set up the data (the Model of the MVC).
+     */
+    private void setupMenuItems() {
+        toppings = Topping.getAvailableToppings();
     }
 }
---
+++ app/src/main/java/com/example/pizzeria/ToppingsAdapter.java
@@ -0,0 +1,91 @@
+package com.example.pizzeria;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+/**
+ * This is an Adapter class to be used to instantiate an adapter for the RecyclerView.
+ * Must extend RecyclerView.Adapter, which will enforce you to implement 3 methods:
+ *      1. onCreateViewHolder, 2. onBindViewHolder, and 3. getItemCount
+ *
+ * You must use the data type <thisClassName.yourHolderName>, in this example
+ * <ItemAdapter.ItemHolder>. This will enforce you to define a constructor for the
+ * ItemAdapter and an inner class ItemsHolder (a static class)
+ * The ItemsHolder class must extend RecyclerView.ViewHolder. In the constructor of this class,
+ * you do something similar to the onCreate() method in an Activity.
+ * @author Lily Chang
+ */
+public class ToppingsAdapter extends RecyclerView.Adapter<ToppingsAdapter.ItemsHolder>{
+    private Context context; //need the context to inflate the layout
+    private ArrayList<Topping> items; //need the data binding to each row of RecyclerView
+
+    public ToppingsAdapter(Context context, ArrayList<Topping> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    /**
+     * This method will inflate the row layout for the items in the RecyclerView
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @NonNull
+    @Override
+    public ItemsHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //inflate the row layout for the items
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.activity_topping, parent, false);
+
+        return new ItemsHolder(view);
+    }
+
+    /**
+     * Assign data values for each row according to their "position" (index) when the item becomes
+     * visible on the screen.
+     * @param holder the instance of ItemsHolder
+     * @param position the index of the item in the list of items
+     */
+    @Override
+    public void onBindViewHolder(@NonNull ItemsHolder holder, int position) {
+        //assign values for each row
+        holder.cbTopping.setText(items.get(position).toString());
+    }
+
+    /**
+     * Get the number of items in the ArrayList.
+     * @return the number of items in the list.
+     */
+    @Override
+    public int getItemCount() {
+        return items.size(); //number of MenuItem in the array list.
+    }
+
+    /**
+     * Get the views from the row layout file, similar to the onCreate() method.
+     */
+    public static class ItemsHolder extends RecyclerView.ViewHolder {
+        private CheckBox cbTopping;
+
+        public ItemsHolder(@NonNull View itemView) {
+            super(itemView);
+            cbTopping = itemView.findViewById(R.id.cbTopping);
+        }
+    }
+}
