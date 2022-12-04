58827edac7f2d7fbfcc91c2bc27269ac69146cb4
Jackson <mabajack@gmail.com>
2022-12-04 00:32:55 -0500

    Pizza adapters

---
+++ app/src/main/java/com/example/pizzeria/CartActivity.java
@@ -0,0 +1,37 @@
+package com.example.pizzeria;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CartActivity extends AppCompatActivity {
+
+    TextView orderNumber;
+    RecyclerView rvCartPizzas;
+    ArrayList<Integer> orderNumberList;
+    ArrayList<Pizza> pizzaList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+        orderNumber = findViewById(R.id.tvOrderNum);
+        orderNumber.setText("Order Number: " + StoreOrder.storeOrder.generateOrderId());
+
+        rvCartPizzas = findViewById(R.id.rvPizzas);
+
+        PizzaAdapter adapter = new PizzaAdapter(this, pizzaList); //create the adapter
+        rvCartPizzas.setAdapter(adapter); //bind the list of items to the RecyclerView
+    }
+
+    private void setPizzaList(){
+        pizzaList = StoreOrder.storeOrder.getCurrentOrder().getPizzaList();
+    }
+
+
+}
